6ee4b04ae9f120bf4d24bcbd56f46ef8ed373245
Nazek Almalaika Altayeb <n.almalaika@yahoo.com>
2025-04-17 19:28:27 +0200

    add Server.java

--- src/main/java/Chat/Server.java
+++ src/main/java/Chat/Server.java
@@ -1,4 +1,48 @@
 package Chat;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * listen to connection requests, those are comming from players, and respond
+ * */
 public class Server {
+    ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer(){
+        try{
+            while(!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+            }
+
+            PlayerHandler playerHandler = new PlayerHandler(socket);
+            Thread thread = new Thread(playerHandler);
+            thread.start();
+
+        } catch (IOException e) {
+            closeServerSocket();
+            // handle exception
+            // throw new RuntimeException(e);
+        }
+    }
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null){
+                serverSocket.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static  void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
 }
