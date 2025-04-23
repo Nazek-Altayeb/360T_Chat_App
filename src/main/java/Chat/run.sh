b940091b27449e9fe58ee9242f583681630f9f11
Nazek Almalaika Altayeb <n.almalaika@yahoo.com>
2025-04-23 14:36:36 +0200

    remove un-necessary libraries

--- src/main/java/Chat/run.sh
+++ src/main/java/Chat/run.sh
@@ -2,41 +2,23 @@
 
 #Current_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #javac ${Current_DIR}/*.java
-#java ${Current_DIR}/Server
 
-
+#compile
+# javac *.java
 
 # start server
-./server.sh &
+wt.exe bash -c "java -classpath . Server.java"
 
-# sleep 3
-# Start the client
-#./player.sh
+sleep 3
 
-# execute the previous command in a new shell window
-# gnome-terminal -- bash -c "!!; exec bash"
-#gnome-terminal -e "bash -c 'echo hello world; sleep 3'"
+# create the initiator (an instance of a Player Class)
+wt.exe bash -c "java -classpath . Player.java"
 
+sleep 1
 
-#gnome-terminal --tab --title="test" --command="bash -c 'cd /player.sh; ls; $SHELL'"
-
-
+# create the receiver (the second instance of the a Player Class)
 wt.exe bash -c "java -classpath . Player.java"
 
 
-
-
-
-# List of commands to run
-#commands=(
- # "echo 'Running command 1'; sleep 5"
-  #"echo 'Running command 2'; sleep 5"
-  #"echo 'Running command 3'; sleep 5"
-#)
-
-# Loop through each command and open it in a new terminal
-#for cmd in "${commands[@]}"; do
- # gnome-terminal -- bash -c "$cmd; exec bash" &
-#done
 
 
